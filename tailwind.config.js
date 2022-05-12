0156f211bebe3049b615157e26c7960d956e1f96
rodifzainulisroi <rodifzainulisroi@gmail.com>
2022-05-12 18:52:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        container:{
+            center:true,
+        },
+        extend: {
+            fontSize: {
+                'xxs': '.5rem',
+                'xs': '.75rem'
+            }
+        },
+    },
+    plugins: [],
+}
